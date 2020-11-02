06d68c25bda13cbf414e29445d9bdbe1c3758c81
Robert Jamborski <jamborski.webdesign@gmail.com>
2020-11-02 14:23:38 +0000

    Animate navigation item active state

--- js/index.js
+++ js/index.js
@@ -3,34 +3,66 @@
 const inputs = document.querySelectorAll('input');
 
 inputs.forEach(input => {
-  input.addEventListener('change', (e) => {
-    e.target.value !== '' 
+  input.addEventListener('change', e => {
+    e.target.value !== ''
       ? e.target.classList.add('form__input--not-empty')
       : e.target.classList.remove('form__input--not-empty');
-  })
-})
-
+  });
+});
 
 // Intersection Observer
 const hero = document.getElementById('home');
+const bussiness = document.getElementById('bussiness');
+const services = document.getElementById('services');
+const reviews = document.getElementById('reviews');
+const contact = document.getElementById('contact');
 
-const onEntry = (entries) => {
-  entries.forEach(entry => { 
+const onEntry = entries => {
+  entries.forEach(entry => {
+    
+    // observe navigation bar
     const nav = document.getElementById('nav');
-    if(entry.isIntersecting) {
+    if (entry.isIntersecting && entry.target.id === 'home') {
       nav.classList.remove('nav--sticky');
-    } else {
-      nav.classList.add('nav--sticky');  
+    } else if (!entry.isIntersecting && entry.target.id === 'home') {
+      nav.classList.add('nav--sticky');
     }
-  })
-}
+  });
+};
 
-const observerOptions = {
+const onEntryItems = entries => {
+  entries.forEach(entry => {
+    
+    // observe navigation items
+    const hash = '#' + entry.target.id;
+    const section = document.querySelector(`a[href="${hash}"]`);
+
+    if (entry.isIntersecting) {
+      section.classList.add('nav__link--active');
+    } else {
+      section.classList.remove('nav__link--active');
+    }  
+  });
+};
+
+const navConfig = {
   root: null,
   rootMargin: '-85% 0px 0px 0px',
-  theshold: []
-}
+  theshold: [],
+};
 
-const observer = new IntersectionObserver(onEntry, observerOptions);
+const menuItemsConfig = {
+  root: null,
+  rootMargin: '-50% 0px -50% 0px',
+  theshold: [],
+};
 
-observer.observe(hero);
+const observerNav = new IntersectionObserver(onEntry, navConfig);
+const observerMenuItems = new IntersectionObserver(onEntryItems, menuItemsConfig);
+
+observerNav.observe(hero);
+observerMenuItems.observe(hero);
+observerMenuItems.observe(bussiness);
+observerMenuItems.observe(services);
+observerMenuItems.observe(reviews);
+observerMenuItems.observe(contact);
